a3acfb2b7020c8cf07b8fe4a696d69e3d99f3140
Ryan O'Donnell <odonnellryanc@gmail.com>
2013-12-24 10:16:41 -0500

    Fixed JS bugs

--- static/js/render_html.js
+++ static/js/render_html.js
@@ -1,22 +1,23 @@
 $(function () {
-
-    $('#post_body').keyup(
+    $('#post_body').on('change keyup paste mouseup',
         function () {
             $.getJSON($SCRIPT_ROOT + '/admin/_render_temp_body/', {
                     post_body: $('textarea[name="post_body"]').val()},
                 function (data) {
                     $('#render_body').html(data.result);
                 });
+            if($('textarea[name="post_body"]').val() == "") {
+                $('#render_body').html("");
+            }
             return false;
         });
 
-    $('#post_title').keyup(
+    $('#post_title').on('change keyup paste mouseup',
         function () {
-            $.getJSON($SCRIPT_ROOT + '/admin/_render_temp_title/', {
-                    post_title: $('input[name="post_title"]').val()},
-                function (data) {
-                    $('#render_title').html(data.result);
-                });
+            $('#render_title').html($('input[name="post_title"]').val());
+            if($('input[name="post_title"]').val() == "") {
+                $('#render_title').html("");
+            }
             return false;
         });
 });
